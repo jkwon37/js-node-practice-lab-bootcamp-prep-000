6e2752bba013a912c684ff546f14d42abc684409
Jonathan Kwon <kwonj56@gmail.com>
2017-10-06 06:46:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 
 var name = "Jonathan"
-var height = 70
+var height = "70"
 
 console.log("Name:")
 console.log(name)
@@ -9,4 +9,4 @@ console.log(name)
 console.log("Height:")
 console.log(height)
 
-console.log(height + 1)
+console.log(parseInt(height) + 1)
